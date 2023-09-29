092f20ab95ce6b0ba935c8ccba7fb885f68b5588
Adriel Teles <adrielteles@outlook.com>
2023-09-29 06:29:17 -0300

    xylophone project

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    var player:AVAudioPlayer!
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundName: sender.currentTitle!)
+    }
+    
+    func playSound(soundName:String){
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+                player = try! AVAudioPlayer(contentsOf: url!)
+                player.play()
+    }
+    
+}
+
